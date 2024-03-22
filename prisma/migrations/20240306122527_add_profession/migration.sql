94c331453afd1e107f5242065674402401402403
Artur Fedcenco <developerar4y@gmail.com>
2024-03-22 14:57:36 +0200

    debug 22.03

--- prisma/migrations/20240306122527_add_profession/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Candidate" ADD COLUMN     "proffesionId" INTEGER;
-
--- AlterTable
-ALTER TABLE "Profession" ADD COLUMN     "candidateId" SERIAL NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Candidate" ADD CONSTRAINT "Candidate_proffesionId_fkey" FOREIGN KEY ("proffesionId") REFERENCES "Profession"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20240309190505_09_03/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Candidate" ALTER COLUMN "locationId" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Partner" ADD COLUMN     "phone" TEXT;
